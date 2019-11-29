0fe530b11c8db6b2818c5796ce531644044986e7
kishanbindal <38564535+kishanbindal@users.noreply.github.com>
2019-11-29 18:41:53 +0530

    Delete distance.py

--- distance.py
+++
@@ -1,9 +0,0 @@
-def userInput():
-
-    x = int(input("Please Enter X co-ordinates :\n"))
-    y = int(input("Please Enter Y co-ordinates :\n"))
-
-    return x,y
-
-def distance(x,y):
-    return (x**2 + y**2)**0.5

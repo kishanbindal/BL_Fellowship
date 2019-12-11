7a8c53d8485ee3c46f1d8ec4072802c3a31e1315
Admin <ingle0608@gmail.com>
2019-12-11 15:30:07 +0530

    Minor Changes to files being committed

--- DataStructures/ordlistprog.py
+++ DataStructures/ordlistprog.py
@@ -9,19 +9,17 @@ def modfile(file, user_num):
         else:
             num_arr = line.split()
     num_arr = [int(num) for num in num_arr]
-    print(num_arr)
     f.close()
     li = OrderedList()
     for num in num_arr:
         li.add(num)
-    li.display()
     if li.search(user_num) is True:
-        x = li.index(user_num)
-        li.pop(x)
+        li.remove(user_num)
     else:
         li.add(user_num)
-    f = open(file, 'r+')
+    f = open(file, 'w')
     for num in li:
         ret_str = ret_str + ' ' + str(num)
+    print(ret_str)
     f.write(ret_str)
     f.close()

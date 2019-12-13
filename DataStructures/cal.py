f75121bf2b0629c2e05986ca6d1f01ffd80be1b5
Kishan Bindal <kishan.bindal@gmail.com>
2019-12-13 10:44:46 +0530

    Small modification in cal.py

--- DataStructures/cal.py
+++ DataStructures/cal.py
@@ -6,7 +6,7 @@ def cal():
     yy = int(input("Please enter year : \n"))
     if mm > 12 or mm < 1:
         raise ValueError("Please enter value of month between 1-12 only.")
-    c = calendar.TextCalendar(calendar.SUNDAY)
-    st = c.formatmonth(2020, 1)
-    print(st)
-    print(calendar.monthcalendar(yy, mm))
+    c = calendar.TextCalendar(calendar.SUNDAY)  # Creating a text calendar and setting the first day to print Sunday
+    st = c.formatmonth(2020, 1)  # inputting the year,month in order to create the calendar
+    print(st)  # printing the calendat
+    return calendar.monthcalendar(yy, mm)  # Returning the 2-D array [week[Dates]]

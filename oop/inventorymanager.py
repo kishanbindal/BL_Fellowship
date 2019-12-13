f2f05425b0108c1f446bd40e151dbd2d75ebbbfd
Kishan <kishanbindal@DESKTOP-NTS6U5P.localdomain>
2019-12-13 22:40:21 +0530

    Committing changes to inventorymanager.py

--- oop/inventorymanager.py
+++ oop/inventorymanager.py
@@ -16,19 +16,28 @@ class InventoryManager:
     def add_to_inventory(self):
         n = int(input("Enter number of items you would like to add to inventory :\n"))
         for i in range(n):
-            n = input("Item you would want to enter to inventory :\n")
-            w = float(input("Enter weight of the product you are adding to inventory :\n"))
-            ppk = float(input("Enter weight of the product "))
-            for item in self.inventory['inventory']:
-                print(item)
-                with open(self.file, 'a+') as f:
-                    data = dict()
-                    data['name'] = n
-                    data['weight'] = w
-                    data['priceperkg'] = ppk
-                    json.dump(data, f, indent=2)
+            while True:
+                try:
+                    n = input("Item you would want to enter to inventory :\n")
+                    w = float(input("Enter weight of the product you are adding to inventory :\n"))
+                    ppk = float(input("Enter cost per KG of the product "))
+
+                except ValueError:
+                    print("Please enter Only int/float values for weight and price per kg")
+
+                else:
+                    break
+
+            with open(self.file, 'r+') as f:
+                data = dict()
+                data['name'] = n
+                data['weight'] = w
+                data['priceperkg'] = ppk
+                self.inventory["inventory"].append(data)
+                json.dump(self.inventory, f, indent=2)
 
 
-im = InventoryManager("inven.json")
-im.inventory_factory()
-im.add_to_inventory()
+if __name__ == "__main__":
+    im = InventoryManager("inven.json")
+    im.inventory_factory()
+    im.add_to_inventory()

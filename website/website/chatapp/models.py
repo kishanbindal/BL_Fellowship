8e087e7e7c79c865b9e59cdfc571258784dd5e82
Kishan Bindal <kishan.bindal@gmail.com>
2020-01-30 11:03:46 +0530

    Removing ChatApp

--- website/website/chatapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class User(models.Model):
-
-    objects = None
-    username = models.CharField(max_length=16, )
-    email = models.EmailField(max_length=32, )
-    password = models.CharField(max_length=16)

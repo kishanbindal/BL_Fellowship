fdcebbc8071675993ceef7dfb411e243a9bc2e0e
Kishan Bindal <kishan.bindal@gmail.com>
2020-02-21 13:30:26 +0530

    Adding Navbar

--- Angular/first-app/src/app/app.module.ts
+++ Angular/first-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {Routes, RouterModule} from '@angular/router'
 
 //------------------Components------------------------
 import { AppComponent } from './app.component';
@@ -9,11 +10,15 @@ import { LoginComponent } from './login/login.component';
 import { UsersidebarComponent } from './usersidebar/usersidebar.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
+
 
 //-------------------Services-------------------------
 import {LoginService} from './services/login-service/login.service'
 import {RegistrationService} from './services/registration-service/registration.service'
-import {ForgotpasswordService} from './services/forgotpass-service/forgotpassword.service'
+import {ForgotpasswordService} from './services/forgotpass-service/forgotpassword.service';
+import { SearchComponent } from './search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -23,12 +28,15 @@ import {ForgotpasswordService} from './services/forgotpass-service/forgotpasswor
     LoginComponent,
     UsersidebarComponent,
     RegisterComponent,
-    ForgotPasswordComponent
-  ],
+    ForgotPasswordComponent,
+    HeaderNavbarComponent,
+    SearchComponent,],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule,
+    BrowserAnimationsModule,
   ],
   providers: [
     LoginService,
